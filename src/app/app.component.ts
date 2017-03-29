7202f1bc07df280ec37bc831531e77f7f8d64fd1
Erica Wright and Sean Peterson <erica@>
2017-03-29 09:01:51 -0700

    add beer class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
 }
+
+export class Beer {
+  empty: boolean = false;
+  ozLeft: number = 1984;
+  constructor(public name: string, public brand: string, public ibu: number, public alcoholContent: number, public halfPintPrice: number, public pintPrice: number) { }
+  
+}
